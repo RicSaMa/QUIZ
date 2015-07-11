f396e4fd7d800ed221fdb3e90e91e0fdc136d6f2
RSM <ricardo.sanz.86@gmail.com>
2015-07-11 20:58:47 +0200

    introduzco tema a las preguntas

--- models/models.js
+++ models/models.js
@@ -39,8 +39,8 @@ exports.Quiz = Quiz;
 sequelize.sync().then(function (){
     Quiz.count().then(function(count){
         if(count === 0) {
-            Quiz.create({pregunta:'Capital de Italia',respuesta:'Roma'});
-            Quiz.create({pregunta:'Capital de Portugal',respuesta:'Lisboa'})
+            Quiz.create({pregunta:'Capital de Italia',respuesta:'Roma',tema:'humanidades'});
+            Quiz.create({pregunta:'Capital de Portugal',respuesta:'Lisboa',tema:'humanidades'})
                 .then(function(){console.log('Base de datos inicializada con dos preguntas')});
         };
     });
--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.param('quizId',quizController.load);
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.put('/quizes/:quizId(\\d+)',quizController.update);
+router.delete('/quizes/:quizId(\\d+)',quizController.destroy);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/quizes/:quizId(\\d+)/edit',quizController.edit);
 router.get('/quizes/new',quizController.new);
