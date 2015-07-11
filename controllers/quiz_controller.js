4aae42a13c8034272c10f76c16f5e47849c0b663
RSM <ricardo.sanz.86@gmail.com>
2015-07-11 17:15:02 +0200

    añado gestion de errores a la creacion de las preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -26,13 +26,13 @@ exports.index = function(req, res){
         texto = texto.replace(new RegExp(" ", 'g'), "%");
         texto = "%"+texto+"%";
         models.Quiz.findAll({where:["pregunta like ?",texto],order:[['pregunta','DESC']]}).then(function (quizes) {
-            res.render('quizes/index.ejs', {quizes: quizes});
+            res.render('quizes/index.ejs', {quizes: quizes, errors: []});
         }).catch(function (error) {
-            res.render('quizes/index.ejs', {quizes: []});
+            res.render('quizes/index.ejs', {quizes: [], errors: []});
         })
     }else{
         models.Quiz.findAll().then(function (quizes) {
-            res.render('quizes/index.ejs', {quizes: quizes});
+            res.render('quizes/index.ejs', {quizes: quizes, errors: []});
         }).catch(function (error) {
             //next(error)
         })
@@ -40,7 +40,7 @@ exports.index = function(req, res){
 };
 
 exports.show = function (req, res){
-    res.render('quizes/show',{quiz: req.quiz});
+    res.render('quizes/show',{quiz: req.quiz, errors: []});
     //models.Quiz.find(req.params.quizId).then(function (quiz){
     //    res.render('quizes/show',{ quiz: quiz});
     //})
@@ -51,24 +51,32 @@ exports.answer = function (req, res){
     if(req.query.respuesta === req.quiz.respuesta){
         resultado = 'Correcto';
     }
-    res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
+    res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 exports.author = function (req, res) {
-    res.render('author');
+    res.render('author', {errors: []});
 };
 
 exports.new = function (req, res){
     var quiz = models.Quiz.build(
         {pregunta:"Pregunta", respuesta:"Respuesta"}
     );
-    res.render('quizes/new',{quiz:quiz});
+    res.render('quizes/new',{quiz:quiz, errors: []});
 };
 
 exports.create = function (req, res){
     var quiz = models.Quiz.build(req.body.quiz);
 
-    quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
-        res.redirect('/quizes');
-    })
+    quiz.validate().then(
+        function (err){
+            if (err){
+                res.render('quizes/new',{quiz:quiz,errors: err.errors});
+            } else {
+                quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
+                    res.redirect('/quizes');
+                })
+            }
+        }
+    );
 };
