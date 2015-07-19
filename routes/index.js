d8d2553a527139b0ada9d7a0c01151ae074c0630
RSM <ricardo.sanz.86@gmail.com>
2015-07-19 13:33:22 +0200

    control de sesiones para edicion de preguntas y comentarios

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/', function(req, res) {
 router.get('/author', quizController.author);
 
 router.param('quizId',quizController.load);
+router.param('commentId',commentController.load);
 
 router.get('/login',sessionController.new);
 router.post('/login',sessionController.create);
@@ -22,14 +23,16 @@ router.get('/logout',sessionController.destroy);
 
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
-router.put('/quizes/:quizId(\\d+)',quizController.update);
-router.delete('/quizes/:quizId(\\d+)',quizController.destroy);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
-router.get('/quizes/:quizId(\\d+)/edit',quizController.edit);
-router.get('/quizes/new',quizController.new);
-router.post('/quizes/create',quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
 
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+    sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
